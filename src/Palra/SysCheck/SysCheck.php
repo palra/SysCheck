178ea867f94d0ca350b872ac1a1373b0fe4360f2
Loïc Payol <loicpayol@gmail.com>
2016-10-14 10:52:58 +0200

    Initialized composer project

---
+++ src/Palra/SysCheck/SysCheck.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Palra\SysCheck;
+
+use Symfony\Component\Console\Application;
+
+/**
+ * Created by PhpStorm.
+ * User: palra
+ * Date: 13/10/16
+ * Time: 16:51
+ */
+class SysCheck extends Application
+{
+    protected static $appName = 'SysCheck';
+    protected static $version = '0.1.0';
+
+    /**
+     * SysCheck constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::$appName, self::$version);
+    }
+}
